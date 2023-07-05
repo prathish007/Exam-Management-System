1ac73d910ae6a7e749103ec75c89d1c4c0730925
prathish <prathismanjini@gmail.com>
2023-07-06 00:12:35 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -10,8 +10,22 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'))
 
-//other funtions
-function saveDetails(details){
+//functions
+function saveHallDetails(details){
+  let i=1;
+  const hallDetails = [];
+  while(details['roomNo'+i]){
+    let o={}
+    o['roomNo']=details['roomNo'+i];
+    o['deptName'] =details['deptName'+i];
+    o['totalSeats']=details['totalSeats'+i];
+    hallDetails.push(o);
+    i++;
+  }
+  fs.writeFileSync(path.join(__dirname,"hallDetails.json"),JSON.stringify(hallDetails));
+}
+
+function saveStudentDetails(details){
   let i=1;
   const studentDetails = [];
   while(details['enrollmentId'+i]){
@@ -28,9 +42,9 @@ function saveDetails(details){
     i++;
   }
   fs.writeFileSync(path.join(__dirname,"studentDetails.json"),JSON.stringify(studentDetails));
-  
-  
-  i=1;
+}
+function saveInvigilatorDetails(details){
+  let i=1;
   const invigilatorDetails = [];
   while(details['staffId'+i]){
     let o={}
@@ -41,9 +55,9 @@ function saveDetails(details){
     i++;
   }
   fs.writeFileSync(path.join(__dirname,"invigilatorDetails.json"),JSON.stringify(invigilatorDetails));
-  
-  
-  i=1;
+}
+function saveCourseDetails(details){
+  let i=1;
   const courseDetails = [];
   while(details['scourseCode'+i]){
     let o={}
@@ -55,23 +69,6 @@ function saveDetails(details){
     i++;
   }
   fs.writeFileSync(path.join(__dirname,"courseDetails.json"),JSON.stringify(courseDetails));
-  
-  
-  
-  i=1;
-  const hallDetails = [];
-  while(details['roomNo'+i]){
-    let o={}
-    o['roomNo']=details['roomNo'+i];
-    o['deptName'] =details['deptName'+i];
-    o['totalSeats']=details['totalSeats'+i];
-    hallDetails.push(o);
-    i++;
-  }
-  fs.writeFileSync(path.join(__dirname,"hallDetails.json"),JSON.stringify(hallDetails));
-
-  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
-
 }
 
 //post methods
@@ -102,55 +99,69 @@ app.post('/getHallTicket',function(req,res){
 
 })
 
+app.post('/proctor',function(req,res){
 
-app.post('/admin',function(req,res){
+  const invigilatorDetails = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
+  let flag=1;
+  for (const val of invigilatorDetails){
+    if(val.staffId === id && val.password === password) {
+      flag=0;
+      break;
+    }
+  }
+  if(flag===1){
+    res.render('login',{flag:flag});
+  }
+  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
+  if(!others.isPublished) flag=2;
+  if(flag===0){
+    const publishedInvigilatorDetails = JSON.parse(fs.readFileSync(path.join(__dirname,"publishedInvigilatorDetails.json")));
+     res.render('invigilator',{exam: publishedInvigilatorDetails,id: id});
+  }
+  else res.render('login',{flag:flag});
+  
+})
+
+app.post('/admin/student',function(req,res){
 const id = req.body.id;
 const password = req.body.password;
 const student = JSON.parse(fs.readFileSync(path.join(__dirname,'studentDetails.json')));
-const invigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
-const course = JSON.parse(fs.readFileSync(path.join(__dirname,'courseDetails.json')));
-const hall = JSON.parse(fs.readFileSync(path.join(__dirname,'hallDetails.json')));
-if(id==='admin' && password=='password123'){
-  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
-  others.isLoggedIn=true;
-  const pstudent = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedStudentDetails.json')));
-  const pinvigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedInvigilatorDetails.json')));
-  fs.writeFileSync(path.join(__dirname,'others.json'),JSON.stringify(others));
-  res.render('admin',{student: student,invigilator: invigilator,course: course,hall: hall,pstudent: pstudent,pinvigilator:pinvigilator,published: others.isPublished}); 
-}
-const invigilatorDetails = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
-let flag=1;
-for (const val of invigilatorDetails){
-  if(val.staffId === id && val.password === password) {
-    flag=0;
-    break;
-  }
-}
-if(flag===1){
-  res.render('login',{flag:flag});
-}
 const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
-if(!others.isPublished) flag=2;
-if(flag===0){
-  const publishedInvigilatorDetails = JSON.parse(fs.readFileSync(path.join(__dirname,"publishedInvigilatorDetails.json")));
-   res.render('invigilator',{exam: publishedInvigilatorDetails,id: id});
+if(id==='admin' && password=='password'){
+  others.isLoggedIn=true;
+  fs.writeFileSync(path.join(__dirname,'others.json'),JSON.stringify(others));
+  res.status(200).render('studentDetails',{student: student}); 
 }
-else res.render('login',{flag:flag});
 
 })
 
 
-app.post('/saveDetails',function(req,res){
-res.redirect('/admin');
-saveDetails(req.body);
+app.post('/saveDetails/student',function(req,res){
+  saveStudentDetails(req.body);
+  res.redirect('/admin/student');
 })
 
+app.post('/saveDetails/invigilator',function(req,res){
+  saveInvigilatorDetails(req.body);
+  res.redirect('/admin/invigilator');
+
+})
+
+app.post('/saveDetails/course',function(req,res){
+  saveCourseDetails(req.body);
+  res.redirect('/admin/course');
+})
+
+app.post('/saveDetails/hall',function(req,res){
+  saveHallDetails(req.body);
+  res.redirect('/admin/hall');
+})
 
 app.post('/logout',function(req,res){
 const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
 others.isLoggedIn = false;
 fs.writeFileSync(path.join(__dirname,'others.json'),JSON.stringify(others));
-res.redirect('/login');
+res.redirect('/adminLogin');
 if(!others.isPublished)
 saveDetails(req.body);
 })
@@ -160,7 +171,11 @@ saveDetails(req.body);
 
 //algorithm for shuffling students based on thier subject code
 app.post("/publish",function(req,res){
-  saveDetails(req.body);
+  let type= req.body.type;
+  if(type===1) saveStudentDetails(req.body);
+  else if(type===2) saveInvigilatorDetails(req.body);
+  else if(type===3) saveCourseDetails(req.body);
+  else if(type===4) saveHallDetails(req.body);
   const others = JSON.parse(fs.readFileSync(path.join(__dirname,"others.json")));
   others.isPublished=true;
   fs.writeFileSync(path.join(__dirname,"others.json"),JSON.stringify(others)); 
@@ -267,66 +282,84 @@ app.post("/publish",function(req,res){
 
 
 
-
-
-
-
-app.post('/unpublish',function(req,res){
-  const student = JSON.parse(fs.readFileSync(path.join(__dirname,'studentDetails.json')));
-  const invigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
-  const course = JSON.parse(fs.readFileSync(path.join(__dirname,'courseDetails.json')));
-  const hall = JSON.parse(fs.readFileSync(path.join(__dirname,'hallDetails.json')));
-  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
+app.get('/unpublish',function(req,res){
+  res.redirect('/admin/student');
+  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json'))); 
   others.isPublished=false;
   fs.writeFileSync(path.join(__dirname,"others.json"),JSON.stringify(others));
-    if(others.isLoggedIn){
-      const pstudent = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedStudentDetails.json')));
-    const pinvigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedInvigilatorDetails.json')));
-      res.render('admin',{student: student,invigilator: invigilator,course: course,hall: hall,pstudent: pstudent,pinvigilator: pinvigilator,published: others.isPublished}); 
-    }
   })
 
 
 
 //get methods
-app.get('/unpublish',function(req,res){
-const student = JSON.parse(fs.readFileSync(path.join(__dirname,'studentDetails.json')));
-const invigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
-const course = JSON.parse(fs.readFileSync(path.join(__dirname,'courseDetails.json')));
-const hall = JSON.parse(fs.readFileSync(path.join(__dirname,'hallDetails.json')));
-const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
-others.isPublished=false;
-fs.writeFileSync(path.join(__dirname,"others.json"),JSON.stringify(others));
-  if(others.isLoggedIn){
-    res.render('admin',{student: student,invigilator: invigilator,course: course,hall: hall,published: others.isPublished}); 
-  }
+
+app.get('/',function(req,res){
+  const filePath =path.join(__dirname,'views','navigator.html');
+  res.status(200).sendFile(filePath);
 })
 
-app.get('/login', function (req, res) {
-  res.render('login',{flag: 0});
+
+app.get('/adminlogin', function (req, res) {
+  res.status(200).render('login',{flag: 0,action: "/admin/student"});
 })
+
+app.get('/invigilatorLogin',function(req,res){
+  res.status(200).render('login',{flag: 0,action:"/invigilator"});
+})
+
 app.get('/studentLogin', function (req, res) {
-  res.render('studentLogin',{flag: 0});
+  res.status(200).render('studentLogin',{flag: 0});
 })
-app.get('/', function (req, res) {
-  res.render('studentLogin',{flag: 0});
-})
-app.get('/admin',function(req,res){
+
+app.get('/admin/student',function(req,res){
   const filePath = path.join(__dirname,'others.json');
-  const data = JSON.parse(fs.readFileSync(filePath));
-  if(!data.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
+  const others = JSON.parse(fs.readFileSync(filePath));
+  if(!others.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
   else{
   const student = JSON.parse(fs.readFileSync(path.join(__dirname,'studentDetails.json')));
-  const invigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
-  const course = JSON.parse(fs.readFileSync(path.join(__dirname,'courseDetails.json')));
-  const hall = JSON.parse(fs.readFileSync(path.join(__dirname,'hallDetails.json')));
-  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
-  const pstudent = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedStudentDetails.json')));
-  const pinvigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedInvigilatorDetails.json')));
-  res.status(200).render('admin',{student: student,invigilator: invigilator,course: course,hall: hall,pstudent:pstudent,pinvigilator:pinvigilator,isSaved: false,published: others.isPublished});
+  res.status(200).render('studentDetails',{student: student});
   }
 })
 
+app.get('/admin/invigilator',function(req,res){
+  const filePath = path.join(__dirname,'others.json');
+  const others = JSON.parse(fs.readFileSync(filePath));
+  if(!others.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
+  else{
+  const invigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'invigilatorDetails.json')));
+  res.status(200).render('invigilatorDetails',{invigilator: invigilator});
+  }
+})
+
+app.get('/admin/course',function(req,res){
+  const filePath = path.join(__dirname,'others.json');
+  const others= JSON.parse(fs.readFileSync(filePath));
+  if(!others.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
+  else{
+  const course = JSON.parse(fs.readFileSync(path.join(__dirname,'courseDetails.json')));
+  res.status(200).render('courseDetails',{course: course});
+  }
+})
+
+app.get('/admin/hall',function(req,res){
+  const filePath = path.join(__dirname,'others.json');
+  const others = JSON.parse(fs.readFileSync(filePath));
+  if(!others.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
+  else{
+  const hall = JSON.parse(fs.readFileSync(path.join(__dirname,'hallDetails.json')));
+  res.status(200).render('hallDetails',{hall: hall});
+  }
+})
+
+app.get('/admin',function(req,res){
+  const others = JSON.parse(fs.readFileSync(path.join(__dirname,'others.json')));
+  if(!others.isLoggedIn) res.status(401).send('<h1>Access Blocked! Unauthorized Access</h1>');
+  else{
+  const pstudent = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedStudentDetails.json')));
+  const pinvigilator = JSON.parse(fs.readFileSync(path.join(__dirname,'publishedInvigilatorDetails.json')));
+  res.status(200).render('publish',{pstudent: pstudent,pinvigilator:pinvigilator,published: others.isPublished}); 
+  }
+})
 
 
 app.listen(3000);
